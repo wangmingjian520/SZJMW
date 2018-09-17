3bf28937b7ec1939161a68a90704ab8c9969758b
lifanglin2018 <3744806@qq.com>
2018-09-17 16:32:43 +0800

    'gs'

--- jmw-app/src/components/NavLeft/index.js
+++ jmw-app/src/components/NavLeft/index.js
@@ -3,12 +3,15 @@ import { Menu } from 'antd';
 import MenuConfig from '../../config/menuConfig'
 import { NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
+import axios from './../../axios'
 import { switchMenu } from './../../redux/action'
+import FaceUrl from '../../utils/apiAndInterfaceUrl'
 const { SubMenu } = Menu;
 
 class NavLeft extends React.Component{
     state = {
-        openKeys:['/main/yjzygl','/main/yjzygl/wzcbgl']
+        currentKey:['/wzmlgl'],
+        openKeys:['/yjzygl','/wzcbgl']
     }
     
     
@@ -23,10 +26,31 @@ class NavLeft extends React.Component{
     }
 
     componentWillMount(){   
+        let menuNodes = "";
+        // axios.jsonp({
+        //     url:FaceUrl.bdApi+FaceUrl.menuUrl,
+        // },function (res) {
+        //     if (res.code == '1') {
+        //         menuNodes = this.renderMenu(res.data);
+        //     } 
+        // })
+        // axios.ajax({
+        //     url:FaceUrl.menuUrl,
+        //     method:FaceUrl.GET,
+        //     baseApi:FaceUrl.bdApi,
+        //     data:{
+        //         isShowLoading:true
+        //     }
+        // }).then((res)=>{
+        //     if(res.code === 0) {
+        //         menuNodes = this.renderMenu(res.data);
+        //     }
+        // })
+
         const menuTreeNode = this.renderMenu(MenuConfig);
         let currentKey = window.location.hash.replace(/#|\?.*$/g,'');
         
-        //console.log("111"+currentKey);
+        console.log(currentKey);
         this.setState({
             currentKey,
             menuTreeNode
