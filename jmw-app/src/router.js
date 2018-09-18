f9ea81b88cbf246a073a3708e308358418e6f4f8
joey <350755594@qq.com>
2018-09-18 17:42:45 +0800

    cb

--- jmw-app/src/router.js
+++ jmw-app/src/router.js
@@ -6,7 +6,7 @@ import Login from './pages/login'
 import NoMatch from './pages/noMatch'
 import Wzmlgl from './pages/yjzygl/wzcbgl/wzmlgl'
 import Wzcbkdgl from './pages/yjzygl/wzcbgl/wzcbkdgl'
-
+import Wzcbxx from './pages/yjzygl/wzcbgl/wzcbxx'
 import Yjsjla from './pages/yjsjcl/yjsjla'
 
 export default class IRouter extends React.Component{
@@ -24,7 +24,7 @@ export default class IRouter extends React.Component{
                             {/* 应急资源管理 */}
                             <Route path="/wzmlgl" component={Wzmlgl}></Route>
                             <Route path="/wzcbkdgl" component={Wzcbkdgl}></Route>
-
+                            <Route path="/wzcbxx" component={Wzcbxx}></Route>
                             <Route path="/yjsjla" component={Yjsjla}></Route>
                             <Redirect to="/wzmlgl" />
                             <Route component={NoMatch}></Route>
