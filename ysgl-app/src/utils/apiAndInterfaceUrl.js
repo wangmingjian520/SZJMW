ed1b4f2b19487c6364f98377bca66439c34c2a4c
lifanglin2018 <3744806@qq.com>
2018-11-02 09:33:42 +0800

    '111'

--- ysgl-app/src/utils/apiAndInterfaceUrl.js
+++ ysgl-app/src/utils/apiAndInterfaceUrl.js
@@ -1,7 +1,7 @@
 export default class ApiAndInterfaceUrl {
     static GET = "GET";
     static POST = "POST";
-    static bdApi = "http://119.147.216.208/api/intrl/bud";
+    static bdApi = "http://192.168.50.29:8060/bud";
     //菜单接口
     static menuUrl = '/platform/listResource';
     //h1接口
